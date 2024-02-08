4688db24095708e0ecdb6672fabb218d5e95c2f0
seabee33 <85791528+seabee33@users.noreply.github.com>
2024-02-08 13:20:05 +1100

    Create index.php

---
+++ Subby/app/settings/index.php
@@ -0,0 +1,29 @@
+<?php require("../functions.php"); loginCheck(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<script src='../../assets/jq.js'></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php name();?> / View All Subscriptions</title>
+	<link rel="stylesheet" href="../assets/appstyles.css">
+</head>
+<body>
+	<div class="appPage">
+
+		<?php echo $appMenu; ?>
+
+		<div class="core">
+
+work in progress (obviously)
+
+		</div>
+
+
+
+
+
+	</div>
+</body>
+<script src='../assets/appScripts.js'></script>
+</html>
